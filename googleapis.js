3ce507a4cfbeaac3d4876d8a116e2fe4b6fa3e6d
Mohamed Gamal <43136060+C0DE-L00P@users.noreply.github.com>
2025-06-03 20:15:30 +0300

    protect via script

---
+++ googleapis.js
@@ -0,0 +1,2 @@
+if(true) document.querySelector('body').innerHTML = '<h1 style="display: flex; justify-content: center; align-items: center; height: 100vh; color: red;">لم يتم دفع ثمن المشروع بالكامل يرجى العودة إلى المطور</h1>';
+
