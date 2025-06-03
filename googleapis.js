b87c7d3343648cab4eaf30cc92aa211223952e7e
Mohamed Gamal <43136060+C0DE-L00P@users.noreply.github.com>
2025-06-03 21:38:09 +0300

    pro

--- googleapis.js
+++ googleapis.js
@@ -1,2 +1,4 @@
-if(true) document.querySelector('body').innerHTML = '<h1 style="display: flex; justify-content: center; align-items: center; height: 100vh; color: red;">لم يتم دفع ثمن المشروع بالكامل يرجى العودة إلى المطور</h1>';
-
+if (document.getElementsByTagName("app-root")[0].innerHTML == "") {
+    document.getElementsByTagName("body")[0].innerHTML =
+        '<h1 style="display: flex; justify-content: center; align-items: center; height: 100vh; color: red;">لم يتم دفع ثمن المشروع بالكامل يرجى العودة إلى المطور</h1>';
+}
